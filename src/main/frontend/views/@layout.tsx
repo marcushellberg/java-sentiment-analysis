558a05432069b4d5bba934450eb561ed8be3837f
start.vaadin.com <no-reply@vaadin.com>
2024-05-08 23:02:28 +0000

    Generated project

---
+++ src/main/frontend/views/@layout.tsx
@@ -0,0 +1,43 @@
+import { createMenuItems, useViewConfig } from '@vaadin/hilla-file-router/runtime.js';
+import { AppLayout, DrawerToggle, Icon, SideNav, SideNavItem } from '@vaadin/react-components';
+import { Suspense, useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+
+const defaultTitle = document.title;
+
+export default function MainLayout() {
+  const currentTitle = useViewConfig()?.title ?? defaultTitle;
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = currentTitle;
+  }, [currentTitle]);
+
+  return (
+    <AppLayout primarySection="drawer">
+      <div slot="drawer" className="flex flex-col justify-between h-full p-m">
+        <header className="flex flex-col gap-m">
+          <h1 className="text-l m-0">My App</h1>
+          <SideNav onNavigate={({ path }) => navigate(path!)} location={location}>
+            {createMenuItems().map(({ to, title, icon }) => (
+              <SideNavItem path={to} key={to}>
+                {icon ? <Icon src={icon} slot="prefix"></Icon> : <></>}
+                {title}
+              </SideNavItem>
+            ))}
+          </SideNav>
+        </header>
+      </div>
+
+      <DrawerToggle slot="navbar" aria-label="Menu toggle"></DrawerToggle>
+      <h2 slot="navbar" className="text-l m-0">
+        {currentTitle}
+      </h2>
+
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </AppLayout>
+  );
+}
